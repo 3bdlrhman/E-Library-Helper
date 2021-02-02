b30f8859318eed7448751aecc73611da1d9ebcfe
abdlrhmanSoudi <abdlrhmansoudi@gmail.com>
2021-02-02 13:34:44 +0200

    add some test cases
    add test case for next endpoints (book search) [Success and Failure]

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,29 +15,36 @@ class flaskr_testcase(unittest.TestCase):
     def tearDown(self):
         ''' excuetes after each test even succes or failed '''
         pass
-    
     def test_get_paginated_books(self):
         res = self.client().get('/books/')
         data = json.loads(res.data)
-        
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_books'])
         self.assertTrue(len(data['books']))
-       
+        
     def test_404_sent_request_beyond_valid_page(self):
         res = self.client().get('/books/?page=11', json={'rating':1})
         data = json.loads(res.data)
-        
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         
     def test_update_book_rating(self):
-        res = self.client().patch('/books/26', json={'rating': 1})
+        res = self.client().patch('/books/16', json={'rating': 5})
         data = json.loads(res.data)
         book = Book.query.filter(Book.id==26).one_or_none()
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(book.format()['rating'], 2)
+        self.assertEqual(data['rating'], 5)
+        
+    def test_get_book_search_with(self):
+        res = self.client().post('/books', json={'search':'Great'})
+        data = json.loads(res.data)
+        self.assertEqual(len(data['books']), 1)
+
+    def test_get_book_search_without(self):
+        res = self.client().post('/books', json={'search':'hipopo'})
+        data = json.loads(res.data)
+        self.assertTrue(data['not_exist'])
         
 if __name__=='__main__':
     unittest.main()
